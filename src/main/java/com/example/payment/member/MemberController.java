1df7ca7a3807d432421fd1c972c1b747f07cf9f7
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-07-08 22:50:18 +0900

    [#1] feat: 회원 조회 api 구현

--- src/main/java/com/example/payment/member/MemberController.java
+++ src/main/java/com/example/payment/member/MemberController.java
@@ -1,10 +1,13 @@
 package com.example.payment.member;
 
+import com.example.payment.member.dto.MemberDto;
 import com.example.payment.member.dto.request.MemberCreateRequest;
 import com.example.payment.member.dto.response.MemberCreateResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,4 +26,9 @@ public class MemberController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberDto> findMember(@PathVariable Long id) {
+        MemberDto response = memberService.findMember(id);
+        return ResponseEntity.ok(response);
+    }
 }
--- src/main/java/com/example/payment/member/MemberService.java
+++ src/main/java/com/example/payment/member/MemberService.java
@@ -1,6 +1,8 @@
 package com.example.payment.member;
 
+import com.example.payment.member.dto.MemberDto;
 import com.example.payment.member.dto.request.MemberCreateRequest;
+import com.example.payment.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +12,12 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public Long createMember(MemberCreateRequest request) {
+    public Long createMember(final MemberCreateRequest request) {
          return memberRepository.save(request.toEntity()).getId();
+    }
+
+    public MemberDto findMember(final Long id) {
+        final Member member = memberRepository.getById(id);
+        return MemberDto.of(member);
     }
 }
