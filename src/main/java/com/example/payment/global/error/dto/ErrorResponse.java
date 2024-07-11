25cb24cbb2b13be033cea79f5b1a00de2867bc1c
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-07-11 13:44:53 +0900

    [#9] refactor : 에러를 응답하는데 사용하는 ErrorResponse Record에 불필요한 생성자 제거

--- src/main/java/com/example/payment/global/error/dto/ErrorResponse.java
+++ src/main/java/com/example/payment/global/error/dto/ErrorResponse.java
@@ -1,8 +1,4 @@
 package com.example.payment.global.error.dto;
 
 public record ErrorResponse(String message) {
-
-    public ErrorResponse(final String message) {
-        this.message = message;
-    }
 }
