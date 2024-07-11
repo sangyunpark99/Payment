8e38047c321b6a7047ff3233da94f08f66184b58
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-07-11 13:42:18 +0900

    [#9] feat: 계좌 생성시 필요한 Resposne Dto 구현

---
+++ src/main/java/com/example/payment/account/dto/response/AccountCreateResponse.java
@@ -0,0 +1,11 @@
+package com.example.payment.account.dto.response;
+
+import com.example.payment.account.dto.AccountDto;
+import java.math.BigDecimal;
+
+public record AccountCreateResponse(String accountNumber, BigDecimal balance) {
+
+    public static AccountCreateResponse getAccountCreateResponse(AccountDto accountDto){
+        return new AccountCreateResponse(accountDto.accountNumber(), accountDto.balance());
+    }
+}
