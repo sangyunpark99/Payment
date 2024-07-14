8d11e971c9e31244b5fa62e12d066bba10b2af92
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-07-14 22:53:54 +0900

    [#11] feat: 계좌 생성, 계좌 정보 불러오기 api 구현

--- src/main/java/com/example/payment/account/AccountController.java
+++ src/main/java/com/example/payment/account/AccountController.java
@@ -2,12 +2,16 @@ package com.example.payment.account;
 
 import com.example.payment.account.dto.request.AccountCreateRequest;
 import com.example.payment.account.dto.response.AccountCreateResponse;
+import com.example.payment.account.dto.response.AccountDetailResponse;
+import com.example.payment.account.dto.response.AccountsDetailsResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +22,20 @@ public class AccountController {
     private final AccountService accountService;
 
     @PostMapping
-    public ResponseEntity<AccountCreateResponse> createAccount(@Valid @RequestBody AccountCreateRequest request) {
-        final AccountCreateResponse response = AccountCreateResponse.getAccountCreateResponse(accountService.createAccount(request));
+    public ResponseEntity<AccountCreateResponse> createAccount(@Valid @RequestBody final AccountCreateRequest request) {
+        final AccountCreateResponse response = AccountCreateResponse.to(accountService.createAccount(request));
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/details")
+    public ResponseEntity<AccountDetailResponse> getAccountDetail(@RequestParam("accountNumber") final String accountNumber) {
+        final AccountDetailResponse response = AccountDetailResponse.to(accountService.getBalance(accountNumber));
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<AccountsDetailsResponse> getAccounts(@RequestParam("email") final String email) {
+        final AccountsDetailsResponse response = new AccountsDetailsResponse(accountService.getAccounts(email));
         return ResponseEntity.ok(response);
     }
 }
