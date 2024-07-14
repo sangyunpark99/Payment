33653583260da310ad28b6fba8cabc62e38e1380
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-07-14 22:57:45 +0900

    [#11] feat: Controller, Service, Repository test코드 작성

--- src/test/java/com/example/payment/account/AccountControllerTest.java
+++ src/test/java/com/example/payment/account/AccountControllerTest.java
@@ -1,7 +1,9 @@
 package com.example.payment.account;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -12,6 +14,8 @@ import com.example.payment.account.dto.response.AccountCreateResponse;
 import com.example.payment.global.error.ControllerAdvice;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -26,7 +30,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@WebMvcTest(controllers = {AccountController.class})
+@WebMvcTest(controllers = AccountController.class)
 public class AccountControllerTest {
 
     @MockBean
@@ -96,5 +100,59 @@ public class AccountControllerTest {
                 Arguments.of(new AccountCreateRequest("abcabc.com", "abc123", null)),
                 Arguments.of(new AccountCreateRequest("abc@abc.com", "abc123", "123"))
         );
+    }
+
+    @Test
+    @DisplayName("계좌의 잔액 확인을 성공한다.")
+    void 계좌의_잔액_확인을_성공한다() throws Exception{
+        //given
+        final String accountNumber = "1234567891";
+        final AccountDto accountDto = new AccountDto(accountNumber, BigDecimal.ZERO);
+
+        //when
+        when(accountService.getBalance(anyString())).thenReturn(accountDto);
+
+        //then
+        mockMvc.perform(get("/api/accounts/details?accountNumber=1234567891")
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("유저가 생성한 계좌들에 대한 목록을 반환받는다.")
+    void 유저가_생성한_계좌들에_대한_목록을_반환받는다() throws Exception{
+        //given
+        List<AccountDto> accounts = new ArrayList<>();
+
+        accounts.add(new AccountDto("1234567891", BigDecimal.ZERO));
+        accounts.add(new AccountDto("1456789123", BigDecimal.ZERO));
+        accounts.add(new AccountDto("2322323233", BigDecimal.ZERO));
+
+        //when
+        when(accountService.getAccounts(anyString())).thenReturn(accounts);
+
+        //then
+        mockMvc.perform(get("/api/accounts?email=abc@abc.com")
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 계좌들에 대한 목록을 반환받지 못한다.")
+    void 존재하지_않는_계좌들에_대한_목록을_반환받지_못한다() throws Exception{
+        //given
+        List<AccountDto> accounts = new ArrayList<>();
+
+        accounts.add(new AccountDto("1234567891", BigDecimal.ZERO));
+        accounts.add(new AccountDto("1456789123", BigDecimal.ZERO));
+        accounts.add(new AccountDto("2322323233", BigDecimal.ZERO));
+
+        //when, then
+        mockMvc.perform(get("/api/accounts?email")
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest());
     }
 }
--- src/test/java/com/example/payment/account/AccountRepositoryTest.java
+++ src/test/java/com/example/payment/account/AccountRepositoryTest.java
@@ -3,11 +3,11 @@ package com.example.payment.account;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.payment.account.entity.Account;
+import com.example.payment.account.exception.NotExistAccountException;
 import com.example.payment.member.MemberRepository;
 import com.example.payment.member.entity.Member;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import java.math.BigDecimal;
+import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,9 +18,6 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 @DataJpaTest
 public class AccountRepositoryTest {
-
-    @PersistenceContext
-    EntityManager em;
 
     @Autowired
     AccountRepository accountRepository;
@@ -92,5 +89,41 @@ public class AccountRepositoryTest {
         Boolean check = accountRepository.existsByAccountNumber(account.getAccountNumber());
 
         assertThat(check).isFalse();
+    }
+
+    @Test
+    @DisplayName("계좌번호로 계좌를 찾는다.")
+    void 계좌번호로_계좌를_찾는다() throws Exception{
+        //given
+
+        final Member member = Member.builder().email("abc@abc.com").nickName("abc").password("abc123").build();
+
+        final Account account = Account.builder()
+                .member(member)
+                .accountNumber("1234567891")
+                .password("1234")
+                .balance(BigDecimal.ZERO)
+                .build();
+
+        //when
+        memberRepository.save(member);
+        accountRepository.save(account);
+        final Account savedAccount = accountRepository.getByAccountNumber(account.getAccountNumber());
+
+        //then
+        SoftAssertions.assertSoftly(softAssertions -> {
+            softAssertions.assertThat(savedAccount.getMember()).isEqualTo(account.getMember());
+            softAssertions.assertThat(savedAccount.getBalance()).isEqualTo(account.getBalance());
+            softAssertions.assertThat(savedAccount.getAccountNumber()).isEqualTo(account.getAccountNumber());
+        });
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 계좌번호로 계좌를 찾아 계좌번호 찾기를 실패한다.")
+    void 존재하지_않는_계좌번호로_계좌를_찾아_계좌번호_찾기를_실패한다() throws Exception{
+
+        //when, then
+        Assertions.assertThatThrownBy(() -> accountRepository.getByAccountNumber("1234567891"))
+                .isInstanceOf(NotExistAccountException.class);
     }
 }
