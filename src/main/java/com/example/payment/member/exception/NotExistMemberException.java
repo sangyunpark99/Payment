941d001a4eb3772f2e012b7dacfa613f5675d226
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-07-14 22:58:51 +0900

    [#11] refactor: 불필요한 필드 변수 제거

--- src/main/java/com/example/payment/member/exception/NotExistMemberException.java
+++ src/main/java/com/example/payment/member/exception/NotExistMemberException.java
@@ -1,13 +1,9 @@
 package com.example.payment.member.exception;
 
 import com.example.payment.global.error.ErrorCode;
-import lombok.Getter;
 
-@Getter
 public class NotExistMemberException extends RuntimeException {
-    private String message;
-
     public NotExistMemberException() {
-        this.message = ErrorCode.NOT_EXIST_USER.getMessage();
+        super(ErrorCode.NOT_EXIST_USER.getMessage());
     }
 }
