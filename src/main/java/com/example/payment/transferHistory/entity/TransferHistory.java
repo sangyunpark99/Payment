15a91a82d401efc21c8b0a9d6739fc2644051128
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-07-15 19:01:34 +0900

    [#13] feat: transferHistory 엔티티 구현

---
+++ src/main/java/com/example/payment/transferHistory/entity/TransferHistory.java
@@ -0,0 +1,48 @@
+package com.example.payment.transferHistory.entity;
+
+import com.example.payment.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TransferHistory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 10)
+    private String withdrawalAccountNumber;
+
+    @Column(length = 10)
+    private String depositAccountNumber;
+
+    private BigDecimal transferAmount;
+
+    private BigDecimal amountAfterWithdrawal;
+
+    private BigDecimal amountAfterDeposit;
+
+    @Builder
+    public TransferHistory(
+            final String withdrawalAccountNumber,
+            final String depositAccountNumber,
+            final BigDecimal transferAmount,
+            final BigDecimal amountAfterWithdrawal,
+            final BigDecimal amountAfterDeposit
+    ) {
+        this.withdrawalAccountNumber = withdrawalAccountNumber;
+        this.depositAccountNumber = depositAccountNumber;
+        this.transferAmount = transferAmount;
+        this.amountAfterWithdrawal = amountAfterWithdrawal;
+        this.amountAfterDeposit = amountAfterDeposit;
+    }
+}
