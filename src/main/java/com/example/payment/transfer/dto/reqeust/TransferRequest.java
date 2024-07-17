13ff44db2e26b4b88e966260bdcd7021b0c66f2e
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-07-17 14:49:52 +0900

    [#13] refactor: 계좌 이체할때 사용하는 Request에서 이체 할 돈의 최소 범위를 1원으로 설정

--- src/main/java/com/example/payment/transfer/dto/reqeust/TransferRequest.java
+++ src/main/java/com/example/payment/transfer/dto/reqeust/TransferRequest.java
@@ -1,5 +1,6 @@
 package com.example.payment.transfer.dto.reqeust;
 
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
@@ -12,7 +13,7 @@ public record TransferRequest(
         @NotBlank(message = "입금 계좌 번호는 필수로 입력해야 합니다.") @Length(min = 10, max = 10)
         String depositAccountNumber,
 
-        @NotNull(message = "이체할 금액은 필수로 입력해야 합니다.")
+        @NotNull(message = "이체할 금액은 필수로 입력해야 합니다.") @DecimalMin("1")
         BigDecimal transferAmount,
 
         @NotNull(message = "계좌의 비밀번호는 필수로 입력해야 합니다.")
