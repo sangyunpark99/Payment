3a7c6f702f2d806c9170000cf607d0b941318d09
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-07-19 17:07:26 +0900

    [#15] feat: 당행 이체시 발생하는 동시성 오류를 해결하기 위한 비관적 락을 적용한 findByAccountNumberForUpdate 메서드 구현

--- src/main/java/com/example/payment/account/AccountRepository.java
+++ src/main/java/com/example/payment/account/AccountRepository.java
@@ -2,8 +2,12 @@ package com.example.payment.account;
 
 import com.example.payment.account.entity.Account;
 import com.example.payment.account.exception.NotExistAccountException;
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,7 +17,15 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Optional<Account> findByAccountNumber(final String accountNumber);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select a from Account a where a.accountNumber = :accountNumber")
+    Optional<Account> findByAccountNumberForUpdate(@Param("accountNumber") String accountNumber);
+
     default Account getByAccountNumber(final String accountNumber) {
         return findByAccountNumber(accountNumber).orElseThrow(NotExistAccountException::new);
+    }
+
+    default Account getByAccountNumberForUpdate(final String accountNumber) {
+        return findByAccountNumberForUpdate(accountNumber).orElseThrow(NotExistAccountException::new);
     }
 }
