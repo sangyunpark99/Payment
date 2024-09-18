dd4c001c751798a2bf3639d721cee394b2ab098e
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-09-19 07:20:30 +0900

    [#24] refactor : accountDto class -> record로 변경

--- src/main/java/com/example/payment/account/dto/AccountDto.java
+++ src/main/java/com/example/payment/account/dto/AccountDto.java
@@ -2,24 +2,8 @@ package com.example.payment.account.dto;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import lombok.Getter;
 
-@Getter
-public class AccountDto {
-
-    private String email;
-    private String accountNumber;
-    private BigDecimal balance;
-    private LocalDateTime registeredAt;
-    private LocalDateTime unregisteredAt;
-
-    public AccountDto(String email, String accountNumber, BigDecimal balance, LocalDateTime registeredAt, LocalDateTime unregisteredAt) {
-        this.email = email;
-        this.accountNumber = accountNumber;
-        this.balance = balance;
-        this.registeredAt = registeredAt;
-        this.unregisteredAt = unregisteredAt;
-    }
+public record AccountDto(String email, String accountNumber, BigDecimal balance, LocalDateTime registeredAt, LocalDateTime unregisteredAt) {
 
     public static AccountDto from(final String email, final String accountNumber, final BigDecimal balance, LocalDateTime registeredAt, LocalDateTime unregisteredAt){
         return new AccountDto(email, accountNumber, balance, registeredAt, unregisteredAt);
