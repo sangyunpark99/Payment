f594130c57fba882749597f465f3b9fd235bc22f
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-09-19 07:18:45 +0900

    [#24] feat: 계좌 해지 응답 dto 구현

--- src/main/java/com/example/payment/account/dto/response/AccountDeleteResponse.java
+++ src/main/java/com/example/payment/account/dto/response/AccountDeleteResponse.java
@@ -2,23 +2,13 @@ package com.example.payment.account.dto.response;
 
 import com.example.payment.account.dto.AccountDto;
 import java.time.LocalDateTime;
-import lombok.Getter;
 
-@Getter
-public class AccountDeleteResponse {
-
-    private String email;
-    private String accountNumber;
-    private LocalDateTime unregisteredAt;
-
-    public AccountDeleteResponse(String email, String accountNumber,
-                                 LocalDateTime unregisteredAt) {
-        this.email = email;
-        this.accountNumber = accountNumber;
-        this.unregisteredAt = unregisteredAt;
-    }
-
+public record AccountDeleteResponse(
+        String email,
+        String accountNumber,
+        LocalDateTime unregisteredAt
+) {
     public static AccountDeleteResponse to(AccountDto accountDto) {
-        return new AccountDeleteResponse(accountDto.getEmail(), accountDto.getAccountNumber(), accountDto.getUnregisteredAt());
+        return new AccountDeleteResponse(accountDto.email(), accountDto.accountNumber(), accountDto.unregisteredAt());
     }
 }
