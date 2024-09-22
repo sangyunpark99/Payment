fa1223a5165a53c3137a4e2c8238d386b66357c5
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-09-22 23:55:29 +0900

    [#28] refactor: 불필요한 import문 제거

--- src/test/java/com/example/payment/transaction/TransactionControllerTest.java
+++ src/test/java/com/example/payment/transaction/TransactionControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.payment.transaction;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
